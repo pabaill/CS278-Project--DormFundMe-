e4f9127252b1ed4b592045f53326ec30fbd85609
pabaill <philip.baillargeon@gmail.com>
2023-05-03 20:29:12 -0700

    Added bottom navigation for smaller screens

--- DormFundMe/src/assets/components/NavBar/dfm-nav-bar.jsx
+++ DormFundMe/src/assets/components/NavBar/dfm-nav-bar.jsx
@@ -1,6 +1,8 @@
-import {AppBar, Toolbar, Typography, IconButton, Tab, Tabs} from '@mui/material';
+import {AppBar, Toolbar, Typography, IconButton, Tab, Tabs, BottomNavigation, BottomNavigationAction} from '@mui/material';
 import {AccountCircle} from '@mui/icons-material'
-import { useState } from 'react';
+import EventIcon from '@mui/icons-material/Event';
+import FeedIcon from '@mui/icons-material/Feed';
+import { useState, useEffect } from 'react';
 import "./dfm-nav-bar.css"
 
 const indexToPage = {
@@ -14,8 +16,19 @@ function DFMNavBar({changePage}) {
         changeTab(tabIndex);
         changePage(indexToPage[tabIndex]);
     };
-    return (
-        <AppBar position="absolute">
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      /* Inside of a "useEffect" hook add an event listener that updates
+        the "width" state variable when the window size changes */
+      window.addEventListener("resize", () => setWidth(window.innerWidth));
+
+      /* passing an empty array as the dependencies of the effect will cause this
+        effect to only run when the component mounts, and not each time it updates.
+        We only want the listener to be added once */
+    }, []);
+    return width > 480 ? (
+          <AppBar position="absolute">
           <Toolbar className='dfm-tool-bar'>
             <Typography variant="h6" noWrap >
               DormFundMe
@@ -37,7 +50,18 @@ function DFMNavBar({changePage}) {
             </div>
           </Toolbar>
         </AppBar>
-    );
+        ) : (
+          <BottomNavigation
+            showLabels
+            value={currTabIndex} 
+            onChange={handleTabChange}
+            className='dfm-bottom-nav'
+          >
+            <BottomNavigationAction label="Feed" icon={<FeedIcon />} />
+            <BottomNavigationAction label="Calendar" icon={<EventIcon />} />
+            <BottomNavigationAction label="Profile" icon={<AccountCircle />} />
+          </BottomNavigation>
+        );
 }
 
 export default DFMNavBar
