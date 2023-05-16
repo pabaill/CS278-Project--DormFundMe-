776e2854f6c08b8d6b6f8c42a383eb44c6fe908f
pabaill <philip.baillargeon@gmail.com>
2023-05-16 10:18:42 -0700

    Change posts to state, remove console logs

--- DormFundMe/src/App.jsx
+++ DormFundMe/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   // Dummy posts for testing
   const sample_desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor at auctor urna nunc. Sit amet luctus venenatis lectus magna. Nisl nunc mi ipsum faucibus. Vel risus commodo viverra maecenas. Leo vel orci porta non pulvinar neque. "
   const stock_img = "https://images.megapixl.com/2219/22193936.jpg";
-  const posts = [{
+  const default_posts = [{
     title: "Floor Dinner",
     upvotes: 10,
     date: new Date("05-09-2023"),
@@ -42,6 +42,7 @@ function App() {
   const theme = createTheme(themeOptions);
   const [currPage, changePage] = useState(document.location.pathname);
   const [isLoggedIn, logIn] = useState(false);
+  const[posts, changePosts] = useState(default_posts);
 
   return (
     <>
@@ -56,7 +57,7 @@ function App() {
             <div className='dfm-page'>
               <Routes path="/">
                   <Route exact path="login" element={<DFMLogin logIn={logIn} changePage={changePage}/>} />
-                  <Route index path="feed" element={<DFMFeed posts={posts} />} />
+                  <Route index path="feed" element={<DFMFeed posts={posts} changePosts={changePosts} />} />
                   <Route path="calendar" element={<DFMCalendar posts={posts} />} />
                   <Route path="profile" element={<DFMProfile logIn={logIn} />} />
                   <Route path="*" element={<Navigate to={isLoggedIn ? "/feed" : "/login"} replace={true} />} />
