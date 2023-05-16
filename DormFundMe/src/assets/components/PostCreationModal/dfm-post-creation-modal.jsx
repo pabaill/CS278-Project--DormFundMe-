958b662f89452af6490d4876b16e8a379da2badb
Caroline Gao <34894152+cggao@users.noreply.github.com>
2023-05-16 11:11:05 -0700

    Format proposal submission

--- DormFundMe/src/assets/components/PostCreationModal/dfm-post-creation-modal.jsx
+++ DormFundMe/src/assets/components/PostCreationModal/dfm-post-creation-modal.jsx
@@ -1,5 +1,5 @@
-import { Typography, Modal, Box, Button, TextField, Select, MenuItem } from '@mui/material';
-import { DatePicker, LocalizationProvider} from '@mui/x-date-pickers';
+import { Typography, Modal, Box, Button, TextField, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import CloseIcon from '@mui/icons-material/Close';
 import "./dfm-post-creation-modal.css";
@@ -15,28 +15,11 @@ function DFMPostCreateModal({ modalOpen, handleOpen }) {
                         </Typography>
                 </header>
                 <CloseIcon onClick={() => handleOpen(false)} className="dfm-post-creation-modal-close"></CloseIcon>
-                <div className='dfm-post-creation-modal-info'>
+                <div className='dfm-post-creation-modal-info-col-1'>
+
                     <TextField className='dfm-post-name-field' label="Event Name" variant='outlined' />
-                    <TextField className='dfm-post-description-field' label="Description" variant='outlined' />
                     <TextField className='dfm-post-location-field' label="Location" variant='outlined' />
-
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker label="Uncontrolled picker" defaultValue={dayjs(new Date())} />
-                    </LocalizationProvider>
-
-                    <TextField className='dfm-post-timeline-field' label="Timeline" variant='outlined' />
                     <TextField className='dfm-post-benefit-field' label="Who would benefit?" variant='outlined' />
-                    <Select
-                        labelId="budget-label"
-                        id="budget-select"
-                        label="Approximate Budgget"
-                        defaultValue={1}
-                    >
-                        <MenuItem value={1}>Small ($0-$99)</MenuItem>
-                        <MenuItem value={2}>Medium ($100-$299)</MenuItem>
-                        <MenuItem value={3}>Large ($300-$999)</MenuItem>
-                        <MenuItem value={4}>Extra Large ($1000+)</MenuItem>
-                    </Select>
                     <input style={{ display: 'none' }} accept="image/*" type="file" id="select-image" />
                     <label htmlFor="select-image">
                         <Button variant="contained" color="primary" component="span">
@@ -44,11 +27,35 @@ function DFMPostCreateModal({ modalOpen, handleOpen }) {
                         </Button>
                     </label>
                 </div>
-                <div className="dfm-post-creation-modal-buttons">
-                    <Button type='submit' variant='outlined'>Submit</Button>
-                    <Button type='submit' variant='outlined' color='warning'>Save Draft</Button>
-                    <Button type='submit' variant='outlined' color='error'>Delete</Button>
+                <div className='dfm-post-creation-modal-info-col-2'>
+                    <TextField className='dfm-post-description-field' label="Description" variant='outlined' />
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker label="Event Date" defaultValue={dayjs(new Date())} />
+                    </LocalizationProvider>
+
+                    <FormControl>
+                        <InputLabel id="budget-label">Approximate Budget</InputLabel>
+                        <Select
+                            labelId="budget-label"
+                            id="budget-select"
+                            label="Approximate Budget"
+                            defaultValue={1}
+                        >
+                            <MenuItem value={1}>Small ($0-$99)</MenuItem>
+                            <MenuItem value={2}>Medium ($100-$299)</MenuItem>
+                            <MenuItem value={3}>Large ($300-$999)</MenuItem>
+                            <MenuItem value={4}>Extra Large ($1000+)</MenuItem>
+                        </Select>
+                    </FormControl>
+
+
+                    <div className="dfm-post-creation-modal-buttons">
+                        <Button type='submit' variant='outlined'>Submit</Button>
+                        <Button type='submit' variant='outlined' color='warning'>Save Draft</Button>
+                        <Button type='submit' variant='outlined' color='error'>Delete</Button>
+                    </div>
                 </div>
+
             </Box>
         </Modal>
     )
