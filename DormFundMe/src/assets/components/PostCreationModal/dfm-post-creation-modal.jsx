4fc3a19290fdc9d06313947ff0db02f4af1ab141
Caroline Gao <34894152+cggao@users.noreply.github.com>
2023-05-16 10:43:02 -0700

    More options in post creation modal

--- DormFundMe/src/assets/components/PostCreationModal/dfm-post-creation-modal.jsx
+++ DormFundMe/src/assets/components/PostCreationModal/dfm-post-creation-modal.jsx
@@ -1,4 +1,6 @@
-import { Typography, Modal, Box, Button, TextField } from '@mui/material';
+import { Typography, Modal, Box, Button, TextField, Select, MenuItem } from '@mui/material';
+import { DatePicker, LocalizationProvider} from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import CloseIcon from '@mui/icons-material/Close';
 import "./dfm-post-creation-modal.css";
 
@@ -17,9 +19,24 @@ function DFMPostCreateModal({ modalOpen, handleOpen }) {
                     <TextField className='dfm-post-name-field' label="Event Name" variant='outlined' />
                     <TextField className='dfm-post-description-field' label="Description" variant='outlined' />
                     <TextField className='dfm-post-location-field' label="Location" variant='outlined' />
+
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker label="Uncontrolled picker" defaultValue={dayjs('2022-04-17')} />
+                    </LocalizationProvider>
+
                     <TextField className='dfm-post-timeline-field' label="Timeline" variant='outlined' />
                     <TextField className='dfm-post-benefit-field' label="Who would benefit?" variant='outlined' />
-                    <TextField className='dfm-post-budget-field' label="Approximate Budget" variant='outlined' />
+                    <Select
+                        labelId="budget-label"
+                        id="budget-select"
+                        label="Approximate Budgget"
+                        defaultValue={1}
+                    >
+                        <MenuItem value={1}>Small ($0-$99)</MenuItem>
+                        <MenuItem value={2}>Medium ($100-$299)</MenuItem>
+                        <MenuItem value={3}>Large ($300-$999)</MenuItem>
+                        <MenuItem value={4}>Extra Large ($1000+)</MenuItem>
+                    </Select>
                     <input style={{ display: 'none' }} accept="image/*" type="file" id="select-image" />
                     <label htmlFor="select-image">
                         <Button variant="contained" color="primary" component="span">
