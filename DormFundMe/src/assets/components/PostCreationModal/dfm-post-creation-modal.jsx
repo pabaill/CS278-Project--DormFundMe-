e110adfe0f048fbb4d2587618bed2949ca0ec906
pabaill <philip.baillargeon@gmail.com>
2023-05-16 11:37:02 -0700

    Shows name of uploaded file

--- DormFundMe/src/assets/components/PostCreationModal/dfm-post-creation-modal.jsx
+++ DormFundMe/src/assets/components/PostCreationModal/dfm-post-creation-modal.jsx
@@ -4,8 +4,16 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import CloseIcon from '@mui/icons-material/Close';
 import "./dfm-post-creation-modal.css";
 import dayjs from 'dayjs';
+import { useState } from 'react';
 
 function DFMPostCreateModal({ modalOpen, handleOpen }) {
+    const [fileUploaded, saveUploadedFile] = useState(undefined);
+    const onFileUpload = (e) => {
+        if (e.target.files) {
+            saveUploadedFile(e.target.files[0].name)
+        }
+    }
+
     return (
         <Modal open={modalOpen} onClose={() => handleOpen(false)}>
             <Box className='dfm-post-creation-modal'>
@@ -20,11 +28,12 @@ function DFMPostCreateModal({ modalOpen, handleOpen }) {
                     <TextField className='dfm-post-name-field' label="Event Name" variant='outlined' />
                     <TextField className='dfm-post-location-field' label="Location" variant='outlined' />
                     <TextField className='dfm-post-benefit-field' label="Who would benefit?" variant='outlined' />
-                    <input style={{ display: 'none' }} accept="image/*" type="file" id="select-image" />
+                    <input style={{ display: 'none' }} accept="image/*" type="file" id="select-image" onChange={(e) => onFileUpload(e)} />
                     <label htmlFor="select-image">
                         <Button variant="contained" color="primary" component="span">
                             Upload Image
                         </Button>
+                        <Typography variant='caption'>{fileUploaded ? fileUploaded : ""}</Typography>
                     </label>
                 </div>
                 <div className='dfm-post-creation-modal-info-col-2'>
