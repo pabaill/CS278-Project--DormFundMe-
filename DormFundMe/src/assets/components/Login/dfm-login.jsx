30b93da6032f4534a0a75e1a0f61aa051d0e8157
pabaill <philip.baillargeon@gmail.com>
2023-05-15 20:17:48 -0700

    Login fields, create account button

--- DormFundMe/src/assets/components/Login/dfm-login.jsx
+++ DormFundMe/src/assets/components/Login/dfm-login.jsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, FormControl, TextField } from "@mui/material";
 import {Link} from "react-router-dom";
 import logo from "./../../images/logo.png";
 import "./dfm-login.css"
@@ -7,7 +7,18 @@ function DFMLogin({logIn, changePage}) {
     return (
         <Box className="dfm-login-box">
             <img src={logo} alt="Dorm Fund Me logo" />
-            <Button LinkComponent={Link} to="/feed" variant="contained" onClick={() => {logIn(true); changePage("/feed")}}>Log In</Button>
+            <FormControl>
+                <div className='dfm-login-input'>
+                    <TextField id="username" label="Username" variant="outlined" />
+                </div>
+                <div className='dfm-login-input'>
+                    <TextField id="password" type="password" label="Password" variant="outlined" />
+                </div>
+            </FormControl>
+            <div className="dfm-login-buttons">
+                <Button style={{margin: "10px"}} LinkComponent={Link} to="/feed" variant="contained" onClick={() => {logIn(true); changePage("/feed")}}>Log In</Button>
+                <Button LinkComponent={Link} to="/feed" variant="contained" onClick={() => {logIn(true); changePage("/profile")}}>Create Account</Button>    
+            </div>
         </Box>
     );
 }
