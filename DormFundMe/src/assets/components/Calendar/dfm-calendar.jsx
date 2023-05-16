3855989d2c5fef51ad741bdd82ca9963699832a1
J0V1K <mr.arifov@gmail.com>
2023-05-16 00:10:38 -0700

    OMFG IT WORKS IM SO HAPPY RIGHT NOW YOU CANT EVEN BELIEVE!!!!!!!!!!!!

--- DormFundMe/src/assets/components/Calendar/dfm-calendar.jsx
+++ DormFundMe/src/assets/components/Calendar/dfm-calendar.jsx
@@ -2,8 +2,13 @@ import {Typography, Paper} from '@mui/material';
 import "./dfm-calendar.css";
 import Calendar from 'react-calendar';
 import React, { useState } from 'react';
+import DFMEventModal from "../EventModal/dfm-event-modal";
+import { Padding } from '@mui/icons-material';
+
 
 const dormname = "Yost";
+
+const dateOptions = {weekday: 'long', month: 'numeric', day: 'numeric'};
 
 function DFMCalendar({posts}) {
     const top = {
@@ -11,11 +16,27 @@ function DFMCalendar({posts}) {
         display: 'flex',
         flexDirection: 'row',
         justifyContent:'center',
-        alignItems:'center'
+        alignItems:'center',
     }
 
-    const makeAlert = (value) => {
-        alert('This date is: ' + value.getMonth() + '/' + value.getDate());
+    const [modalOpen, handleOpen] = useState(false);
+    const [count, setCount] = useState(0);
+
+    const checkForEvents = (value) => {
+        let array = posts
+        let date1 = value
+        console.log(value)
+        for (let i = 0; i < array.length; i++) {
+            let date2 = array[i].date
+            if (date1 > date2) {
+                console.log("Date 1 is greater than Date 2");
+            } else if (date1 < date2) {
+                console.log("Date 1 is less than Date 2");
+            } else {
+                handleOpen(true)
+                setCount(i)
+            }
+        }
     }
 
     // hard coded values for first quarter
@@ -31,19 +52,19 @@ function DFMCalendar({posts}) {
     
     const [value, onChange] = useState(new Date());
     return (
-        // TODO: "Add posts" button/menu, filtering of posts
+        <div>
         <Paper className='dfm-feed-paper'>
             <Typography variant='h4'>Calendar for {dormname}</Typography>
             <div style={top}>
                 <Calendar
                     className="react-calendar"
                     onChange={onChange} 
-                    value={value} 
+                    value = {value} 
                     minDate={firstDayF}
                     maxDate={lastDayF}
                     minDetail="year"
                     showNeighboringMonth={false}
-                    onClickDay={(value) => makeAlert(value)} 
+                    onClickDay={(value) => checkForEvents(value)} 
                 />
                 <Calendar 
                     onChange={onChange} 
@@ -52,7 +73,7 @@ function DFMCalendar({posts}) {
                     maxDate={lastDayS}
                     minDetail="year"
                     showNeighboringMonth={false}
-                    onClickDay={(value) => makeAlert(value)} 
+                    onClickDay={(value) => checkForEvents(value)} 
                 />
                 <Calendar 
                     onChange={onChange}  
@@ -61,10 +82,14 @@ function DFMCalendar({posts}) {
                     maxDate={lastDayT}
                     minDetail="year"
                     showNeighboringMonth={false}
-                    onClickDay={(value) => makeAlert(value)} 
+                    onClickDay={(value) => checkForEvents(value)} 
                 />
             </div>
+            
         </Paper>
+        <DFMEventModal post={posts[count]} modalOpen={modalOpen} handleOpen={handleOpen} dateOptions={dateOptions} />
+
+        </div>
     );
 }
 
