e2cfca2f640a40deef082c0f3b1dbf55e803e041
pabaill <philip.baillargeon@gmail.com>
2023-06-04 13:57:23 -0700

    Static rules card

--- DormFundMe/src/assets/components/Feed/dfm-feed.jsx
+++ DormFundMe/src/assets/components/Feed/dfm-feed.jsx
@@ -1,4 +1,4 @@
-import { Button, Typography, Paper } from '@mui/material';
+import { Button, Typography, Paper, Box } from '@mui/material';
 import "./dfm-feed.css";
 import DFMPost from "../Post/dfm-post";
 import { useEffect, useState } from 'react';
@@ -23,24 +23,38 @@ function DFMFeed({posts, changePosts, user}) {
     }, [user])
 
     return (
-        // TODO: "Add posts" button/menu, filtering of posts
-        <Paper className='dfm-feed-paper'>
-            <Typography variant='h4'>Trending in {dormname}</Typography>
-            <Button variant="contained" id="New Post" onClick={() => handleOpen(true)}> 
-                New Post
-            </Button>
-            <ul className='dfm-feed-post-list'>
-                {Object.values(posts).sort((a,b) => b.date - a.date).map(p => (
-                    <li key={p._id} className='dfm-feed-post-list-item'>
-                        <DFMPost user={user} post={p}></DFMPost>
-                    </li>
-                ))}
-            </ul>
-            <DFMPostCreateModal user={user} modalOpen={modalOpen} handleOpen={handleOpen} changePosts={changePosts} posts={posts} />
-            {post_id && post_id < posts.length ? (
-                <DFMEventModal user={user} post={posts[post_id]} modalOpen={linkedOpen} handleOpen={handleLinkedOpen} dateOptions={dateOptions} ></DFMEventModal>
-            ) : (<div />)}
-        </Paper>
+        <div className='dfm-feed-wrapper'>
+            <Paper className='dfm-feed-paper'>
+                <Typography variant='h4'>Trending in {dormname}</Typography>
+                <Button variant="contained" id="New Post" onClick={() => handleOpen(true)}> 
+                    New Post
+                </Button>
+                <ul className='dfm-feed-post-list'>
+                    {Object.values(posts).sort((a,b) => b.date - a.date).map(p => (
+                        <li key={p._id} className='dfm-feed-post-list-item'>
+                            <DFMPost user={user} post={p}></DFMPost>
+                        </li>
+                    ))}
+                </ul>
+                <DFMPostCreateModal user={user} modalOpen={modalOpen} handleOpen={handleOpen} changePosts={changePosts} posts={posts} />
+                {post_id && post_id < posts.length ? (
+                    <DFMEventModal user={user} post={posts[post_id]} modalOpen={linkedOpen} handleOpen={handleLinkedOpen} dateOptions={dateOptions} ></DFMEventModal>
+                ) : (<div />)}
+            </Paper>
+            <Paper className='dfm-exp-paper'>
+                <Box>
+                    <Typography variant='h5'>Expectations</Typography>
+                    <Typography variant='body1'>Welcome to the {dormname} DormFundMe Community!</Typography>
+                    <Typography variant='body2' align='left'>
+                        <ol>
+                            <li>Post ideas that are realistic and accessible to everyone.</li>
+                            <li>Hate speech and harassment will not be tolerated. Flag posts that feature harmful content to remove them. </li>
+                            <li>On the platform, you will only be identifiable by your username. If a harmful post from your account is flagged, <b>your identity will be revealed</b> and you will face consequences as a resident of {dormname}. </li>
+                        </ol>
+                    </Typography>
+                </Box>
+            </Paper>
+        </div>
     );
 }
 
